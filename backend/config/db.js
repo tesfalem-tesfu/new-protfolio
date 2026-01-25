e2dd8c6ae32c5eb9f827df231150ab319c0f8aa8
tesfalem-tesfu <tesfutesfalemmarkos@gmail.com>
2026-01-25 15:11:31 +0300

    Update Contact component and db configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect(err => {
@@ -13,7 +14,7 @@ db.connect(err => {
     console.error("❌ MySQL connection failed:", err);
     return;
   }
-  console.log("✅ MySQL Connected to portfol database");
+  console.log("✅ MySQL Connected to Railway MySQL");
 });
 
 module.exports = db;
